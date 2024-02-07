0446ad4d5a5b26af8b18cfb2ffffd15032a936ed
Hazel Garro <hazelvaleriagarroarias@gmail.com>
2024-02-07 14:57:49 -0600

    cambio en el modelo Cliente

--- APIHotelBeach/Models/Cliente.cs
+++ APIHotelBeach/Models/Cliente.cs
@@ -7,32 +7,37 @@ namespace APIHotelBeach.Models
 
         [Key]
         [Required]
-        public int Id { get; set; }
-
-        [Required]
-        [MaxLength(25)]
-        [MinLength(8)]
         public string Cedula { get; set; }
 
         [Required]
-        [MinLength(5)]
         public string TipoCedula { get; set; }
 
         [Required]
-        [StringLength(50)]
         public string NombreCompleto { get; set; }
 
         [Required]
-        [StringLength(15)]
         public string Telefono { get; set; }
 
         [Required]
-        [StringLength(50)]
         public string Direccion { get; set; }
 
         [Required]
-        [StringLength(50)]
         public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public int TipoUsuario { get; set; }
+
+        [Required]
+        public int Restablecer { get; set; }
+
+        [Required]
+        public DateTime FechaRegistro { get; set; }
+
+        [Required]
+        public int Estado { get; set; }
 
     }
 }
