8923482ebe5cbe5eeac48ce8e01399cb9bf2ded3
artaviafloresmonica <artaviafloresmonica@gmail.com>
2024-02-13 19:01:42 -0600

    Se agregó el objeto Cheque

---
+++ APIHotelBeach/Models/Cheque.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIHotelBeach.Models
+{
+    public class Cheque
+    {
+        [Key]
+        public int NumeroCheque { get; set; }
+
+        public string NombreBanco { get; set; }
+
+        public int IdReservacion { get; set; }
+    }
+}
