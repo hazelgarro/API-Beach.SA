d95e2f54bccf40076b9c1b9276da8c90dba8c203
Jennifer Rodriguez <jenniroes2801@gmail.com>
2024-02-14 16:15:47 -0600

    Métodos Create, métodos relacionados a Password y email en UsuariosController. Además, se agregó el modelo para restablecer contraseña

---
+++ APIHotelBeach/Controllers/Restablecer.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APIHotelBeach.Models
+{
+    public class Restablecer
+    {
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la contraseña enviada por email")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar su nueva contraseña")]
+        public string NuevoPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirme su nueva contraseña")]
+        public string Confirmar { get; set; }
+    }
+}
