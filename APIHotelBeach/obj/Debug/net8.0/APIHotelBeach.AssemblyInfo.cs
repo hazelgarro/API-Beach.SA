ca6014349e8fa891c9adfe72b284044152be45f2
dianaartavia <diana.artavia.da@gmail.com>
2024-02-26 10:10:36 -0600

    Publicacion de la api

--- APIHotelBeach/obj/Debug/net8.0/APIHotelBeach.AssemblyInfo.cs
+++ APIHotelBeach/obj/Debug/net8.0/APIHotelBeach.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIHotelBeach")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256364d41d5aa388ba0f0f2b2faab6f5f0d50862")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIHotelBeach")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIHotelBeach")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
